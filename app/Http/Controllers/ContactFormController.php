27cf9759cd617a7ae9d6a1b20b3cafada1f5ff41
Hashmaryne van Cuylenburg <hashmaryne@outlook.com>
2020-06-10 14:34:24 +0530

    contact form

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -30,5 +30,7 @@ class ContactFormController extends Controller
         ]);
 
         Mail::to('sithivili.project@gmail.com')->send(new ContactFormMail($data));
+        return redirect('contact')
+        ->with('message', 'Thanks for your message. We\'ll be in touch.');
     }
 }
