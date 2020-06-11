a07b180360ca91c374fa7ba9e0fd96aa71e44119
Hashmaryne van Cuylenburg <hashmaryne@outlook.com>
2020-06-11 09:49:46 +0530

    contact

--- app/Mail/ContactFormMail.php
+++ app/Mail/ContactFormMail.php
@@ -10,7 +10,7 @@ use Illuminate\Queue\SerializesModels;
 class ContactFormMail extends Mailable
 {
     use Queueable, SerializesModels;
-
+    public $data;
     /**
      * Create a new message instance.
      *
