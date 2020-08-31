99b5709a7bc251ac62226acc7a5698903aae5a32
Hashmaryne <hashmaryne@gmail.com>
2020-08-31 15:07:41 +0530

    seo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Free Web tutorials">
+    <meta name="keywords" content="HTML, CSS, JavaScript">
+    <meta name="author" content="John Doe">
+    <meta name="google-site-verification" content="your verification string">
     @include('meta::manager', [
     'title'         => 'Project Sithivili',
     'description'   => 'Project Sithivili is an Annoymous Emotional support application to improve the mental health of Sri Lankans',
